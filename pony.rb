46008f4e77f042d99b3eeff5302b1bf2aac95ece
Dirk Breuer <mail@breuer.io>
2019-01-24 14:32:16 +0100

    ponies can run

--- pony.rb
+++ pony.rb
@@ -6,5 +6,18 @@ class Pony
   def initialize(name:, color:)
     @name = name
     @color = color
+    @state = :idle
+  end
+
+  def run
+    @state = :running
+  end
+
+  def idle?
+    @state == :idle
+  end
+
+  def running?
+    @state == :running
   end
 end
